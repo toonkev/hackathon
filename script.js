1a6c9eae8b752afc80be8524957a365b90964565
William Park <galaxiapark07@gmail.com>
2023-07-01 16:37:06 +1000

    fixed map.html and added testing.html to see if a program outputs a map correctly

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: { lat: -34.397, lng: 150.644 },
-    });
-  }
-  
-  
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -25.363882, lng: 131.044922 }
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: -25.363882, lng: 131.044922 },
+    map: map
+  });
+}
