d240d1d5fa780fa0dff529b0bb027dcd917de8f8
William Park <galaxiapark07@gmail.com>
2023-07-01 18:23:13 +1000

    can enter the address

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
-let map, infoWindow;
+let map, infoWindow, marker, geocoder, responseDiv, response;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 10,
+    zoom: 8,
+    mapTypeControl: false,
   });
+
+  geocoder = new google.maps.Geocoder();
   infoWindow = new google.maps.InfoWindow();
+  
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.placeholder = "Enter a location";
 
+  const submitButton = document.createElement("input");
+  submitButton.type = "button";
+  submitButton.value = "Geocode";
+  submitButton.classList.add("button", "button-primary");
+
+  const clearButton = document.createElement("input");
+  clearButton.type = "button";
+  clearButton.value = "Clear";
+  clearButton.classList.add("button", "button-secondary");
+  
+  response = document.createElement("pre");
+  response.id = "response";
+  response.innerText = "";
+
+  responseDiv = document.createElement("div");
+  responseDiv.id = "response-container";
+  responseDiv.appendChild(response);
+
+  const instructionsElement = document.createElement("p");
+  instructionsElement.id = "instructions";
+  instructionsElement.innerHTML = "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
+  
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(instructionsElement);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+  
+  marker = new google.maps.Marker({
+    map,
+  });
+  
+  map.addListener("click", (e) => {
+    geocode({ location: e.latLng });
+  });
+
+  submitButton.addEventListener("click", () =>
+    geocode({ address: inputText.value })
+  );
+  
+  clearButton.addEventListener("click", () => {
+    clear();
+  });
+  
+  clear();
+  
   const locationButton = document.createElement("button");
-
   locationButton.textContent = "Pan to Current Location";
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  
   locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -32,7 +84,6 @@ function initMap() {
         }
       );
     } else {
-      // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
   });
@@ -46,6 +97,30 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       : "Error: Your browser doesn't support geolocation."
   );
   infoWindow.open(map);
+}
+
+function clear() {
+  marker.setMap(null);
+  responseDiv.style.display = "none";
+}
+
+function geocode(request) {
+  clear();
+  geocoder
+    .geocode(request)
+    .then((result) => {
+      const { results } = result;
+
+      map.setCenter(results[0].geometry.location);
+      marker.setPosition(results[0].geometry.location);
+      marker.setMap(map);
+      responseDiv.style.display = "block";
+      response.innerText = JSON.stringify(result, null, 2);
+      return results;
+    })
+    .catch((e) => {
+      alert("Geocode was not successful for the following reason: " + e);
+    });
 }
 
 window.initMap = initMap;
